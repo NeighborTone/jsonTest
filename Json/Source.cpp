df9d341945bebacfc43f709ce7db7d03ef6c7a0e
tonarinohito <rsp44316@gmail.com>
2018-09-22 04:12:49 +0900

    追加

--- Json/Source.cpp
+++ Json/Source.cpp
@@ -1,10 +1,27 @@
 #include "JsonRead.hpp"
 
+struct Player
+{
+	int hp;
+	int atk;
+	bool isHuman;
+	void Show()
+	{
+		std::cout << "HP:" << hp << std::endl;
+		std::cout << "ATK:" << atk << std::endl;
+		std::cout << "Human?:" << std::boolalpha << isHuman << std::endl;
+	}
+};
 //¦JSON‚É‚ÍƒRƒƒ“ƒg•¶‚Í‚È‚¢
 int main()
 {
+	Player player;
 	JsonRead json;
 	json.Load("test.json");
-	std::cout << json.GetParameter<number>("Player", "hp");
+	player.hp = static_cast<int>(json.GetParameter<number>("Player", "hp"));
+	player.atk = static_cast<int>(json.GetParameter<number>("Player", "atk"));
+	player.isHuman = json.GetParameter<bool>("Player", "human");
+
+	player.Show();
 	system("pause");
 }
