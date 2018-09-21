62a34f11e90c627078d6bb520efd7b49d670fc35
tonarinohito <rsp44316@gmail.com>
2018-09-22 04:03:59 +0900

    簡易的にクラス化してみた

---
+++ Json/JsonRead.hpp
@@ -0,0 +1,70 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <assert.h>
+#include "picojson.h"
+
+using number = double;
+
+class JsonRead final
+{
+private:
+	picojson::value v_;
+public:
+	JsonRead() = default;
+	JsonRead(const std::string& path)
+	{
+		// JSONデータの読み込み。
+		std::stringstream ss;
+		std::ifstream ifs(path, std::ios::in);
+		if (ifs.fail())
+		{
+			std::cerr << "failed to read test.json" << std::endl;
+		}
+		ss << ifs.rdbuf();
+		ifs.close();
+
+		//JSONデータを解析する。
+		const std::string err = picojson::parse(v_, ss); 
+		if (err.empty() == false)
+		{
+			std::cerr << err << std::endl;
+		}
+	}
+	bool Load(const std::string& path)
+	{
+		// JSONデータの読み込み。
+		std::stringstream ss;
+		std::ifstream ifs(path, std::ios::in);
+		if (ifs.fail())
+		{
+			return false;
+		}
+		ss << ifs.rdbuf();
+		ifs.close();
+
+		//JSONデータを解析する。
+		const std::string err = picojson::parse(v_, ss);
+		if (!err.empty())
+		{
+			return false;
+		}
+		return true;
+	}
+
+	template <class T>
+	const T GetParameter(const std::string& name)
+	{
+		picojson::object obj_ = v_.get<picojson::object>();
+		return obj_[name].get<T>();
+	}
+
+	template <class T>
+	const T GetParameter(const std::string& objectName, const std::string& name)
+	{
+		picojson::object obj_ =  v_.get<picojson::object>()[objectName].get<picojson::object>();
+		return obj_[name].get<T>();
+	}
+};
--- Json/Source.cpp
+++ Json/Source.cpp
@@ -1,60 +1,10 @@
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <fstream>
-
-
-#include "picojson.h"
-
-template <class T>
-const T GetParameter(const std::string& name,
-	picojson::object& jsonObj)
-{
-	return jsonObj[name].get<T>();
-}
+#include "JsonRead.hpp"
 
 //※JSONにはコメント文はない
 int main()
 {
-	// JSONデータの読み込み。
-	std::stringstream ss;
-	std::ifstream ifs("test.json", std::ios::in);
-	if (ifs.fail()) 
-	{
-		std::cerr << "failed to read test.json" << std::endl;
-	}
-	ss << ifs.rdbuf();
-	ifs.close();
-
-	// JSONデータを解析する。
-	picojson::value v;	//この変数に内容がすべて入る
-	const std::string err = picojson::parse(v, ss); //内容を取り込む
-	if (err.empty() == false)
-	{
-		std::cerr << err << std::endl;
-	}
-
-	//パースされたデータを取得
-	picojson::object& obj = v.get<picojson::object>()["Player"].get<picojson::object>();
-	auto name = obj["name"].get<std::string>();
-	std::cout << "name: " << name << std::endl;
-	//jsonは数値はすべてdoubleで扱う
-	//intやfloatだと未解決の外部シンボルとなる
-	using number = double;
-	auto hp = obj["hp"].get<number>();
-	std::cout << "hp: " << static_cast<int>(hp) << std::endl;
-	auto atk = obj["atk"].get<number>();
-	std::cout << "atk: " << static_cast<float>(atk) << std::endl;
-	auto isHuman = obj["human"].get<bool>();
-	std::cout << "human: " << std::boolalpha << isHuman << std::endl;
-	//配列の取得
-	auto arr = obj["array"].get<picojson::array>();
-	for (const auto& it : arr)
-	{
-		//配列の中身を取得する
-		auto num = it.get<number>();
-		std::cout << "array: " << num << std::endl;
-	}
-
+	JsonRead json;
+	json.Load("test.json");
+	std::cout << json.GetParameter<number>("Player", "hp");
 	system("pause");
 }
