7a2d7a899027073296dceef7fb13021d258e61de
tonarinohito <rsp44316@gmail.com>
2018-09-22 05:07:21 +0900

    修正

--- Json/Source.cpp
+++ Json/Source.cpp
@@ -10,6 +10,7 @@ private:
 	std::string sex;
 	std::string age;
 	std::string job;
+	std::string data[3];
 public:
 	Player()
 	{
@@ -19,20 +20,26 @@ public:
 		hp = static_cast<int>(json.GetParameter<number>("Player", "hp"));
 		atk = static_cast<int>(json.GetParameter<number>("Player", "atk"));
 		isHuman = json.GetParameter<bool>("Player", "human");
-		//‘å•ª–³—‚â‚è‚ ‚Æ‚Å’¼‚·‚©‚à
-		sex = json.GetArrayParameter<std::string>("Player", "data",0);
-		age = json.GetArrayParameter<std::string>("Player", "data",1);
-		job = json.GetArrayParameter<std::string>("Player", "data",2);
+		//‘å•ª–³—‚â‚è‚ ‚Æ‚Å’¼‚·
+		sex = json.GetParameter<std::string>("Player", "data", 0);
+		age = json.GetParameter<std::string>("Player", "data", 1);
+		job = json.GetParameter<std::string>("Player", "data", 2);
+		//”z—ñ‚Ìæ“¾
+		json.GetArrayParameter<std::string>("Player", "data", data ,std::size(data));
 	}
 	void Show()
 	{
 		std::cout << "Name:" << name << std::endl;
 		std::cout << "HP:" << hp << std::endl;
 		std::cout << "ATK:" << atk << std::endl;
-		std::cout << "Human?:" << std::boolalpha << isHuman << std::endl;
+		std::cout << "isHuman:" << std::boolalpha << isHuman << std::endl;
 		std::cout << "Sex:" << sex << std::endl;
 		std::cout << "Age:" << age << std::endl;
 		std::cout << "Job:" << job << std::endl;
+		for (const auto& it : data)
+		{
+			std::cout << "Data:" << it << std::endl;
+		}
 	}
 };
 //¦JSON‚É‚ÍƒRƒƒ“ƒg•¶‚Í‚È‚¢
